2b9795191cec986000ea927a3bfe7beb9f766fed
cvnxsolution <cvnxsolution@gmail.com>
2025-12-05 19:30:46 +0630

    feat(backend): initial project creation

---
+++ medmy-backend/src/main/java/org/cvnxsolution/medmybackend/MedmyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.cvnxsolution.medmybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedmyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedmyBackendApplication.class, args);
+    }
+
+}
---
+++ medmy-backend/src/test/java/org/cvnxsolution/medmybackend/MedmyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cvnxsolution.medmybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedmyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
